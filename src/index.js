47cc930499c02e63529d8ab77a5ab2aa49bc860a
Kevin Thompson <kevinthompson.nmtcs@gmail.com>
2020-06-24 20:48:26 -0400

    fixed index.js script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import _ from 'lodash';
+(function(){
+    const div = document.querySelector('.content');
+    const h1 = document.createElement('h1');
 
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+    h1.textContent = 'Hello World';
+    div.appendChild(h1);
+})();
